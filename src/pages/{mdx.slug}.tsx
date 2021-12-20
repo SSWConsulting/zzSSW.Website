068dfb622b88d8df837613c81c694858832e5a7c
pfmao <ErenJagerM@outlook.com>
2021-12-20 14:49:44 +0800

    feat: create dynamic routing with slug

---
+++ src/pages/{mdx.slug}.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Layout from '../components/layout/layout';
+
+const Page = ({ data }) => {
+    const post = data.mdx;
+    const {
+        frontmatter: { title },
+        body,
+    } = post;
+
+    return (
+        <Layout pageTitle={title}>
+            <>
+                <h1>{title}</h1>
+                <MDXRenderer>{body}</MDXRenderer>
+            </>
+        </Layout>
+    );
+};
+
+export const query = graphql`
+    query PageById($id: String) {
+        mdx(id: { eq: $id }) {
+            frontmatter {
+                title
+            }
+            body
+        }
+    }
+`;
+
+export default Page;
