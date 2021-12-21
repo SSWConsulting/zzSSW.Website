f4f877c116edc2fec4c06a268c4da4836768f38d
pfmao <ErenJagerM@outlook.com>
2021-12-21 09:41:22 +0800

    feat: add consulting services page

---
+++ src/pages/consulting.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const ConsultingServices = () => {
+    return (
+        <Layout pageTitle="SSW Consulting - .NET, Web, Mobile, CRM, SharePoint, Azure, Power BI, Angular, React, Office 365 and Dynamics">
+            {/* TODO: Content */}
+            <h1>Consulting Services</h1>
+        </Layout>
+    );
+};
+
+export default ConsultingServices;
