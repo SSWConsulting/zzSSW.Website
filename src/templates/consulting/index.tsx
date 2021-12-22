67243e1f31d09a5ccc927da54122566005c2cd99
pfmao <ErenJagerM@outlook.com>
2021-12-22 09:06:53 +0800

    feat: extract booking component

--- src/templates/consulting/index.tsx
+++ src/templates/consulting/index.tsx
@@ -3,8 +3,11 @@ import { StaticImage } from 'gatsby-plugin-image';
 
 import Layout from '../../components/layout';
 import Breadcrumb from '../../components/breadcrumb';
-import MVC_BACKGROUND from '../../videos/MVC_background.mp4';
-import './index.css';
+// import MVC_BACKGROUND from '../../videos/MVC_background.mp4';
+import Booking from './components/booking';
+
+// TODO: This css file is for reference, delete it later
+// import './index.css';
 
 const Consulting = ({ data: { title } }) => {
     const data = [
@@ -26,39 +29,15 @@ const Consulting = ({ data: { title } }) => {
             <>
                 <Breadcrumb data={data} />
 
-                <div className="Consulting-Hero employement-video">
-                    <div className="container hero">
-                        <h1>
-                            Want to build your web application in{' '}
-                            <span className="red">React</span>?
-                        </h1>
-                        <h2>
-                            Easy to learn, easy to use and easy to be awesome
-                        </h2>
+                <Booking />
 
-                        <button
-                            type="button"
-                            className="button raised hoverable"
-                            data-toggle="modal"
-                            data-target="#myModal"
-                        >
-                            <div className="anim"></div>
-                            Book a FREE Initial Meeting
-                        </button>
-                        <h2>or call us on +61 2 9953 3000</h2>
-                    </div>
-
-                    <div className="arrow-scroll bounce">
-                        <a className="fa fa-chevron-down" href="#more"></a>
-                    </div>
-                </div>
-
-                <div id="videocontainer">
+                {/* TODO: Add video background */}
+                {/* <div id="videocontainer">
                     <video autoPlay muted loop>
                         <source src={MVC_BACKGROUND} type="video/mp4" />
                         Your browser does not support HTML5 video.
                     </video>
-                </div>
+                </div> */}
 
                 <div className="benefits-section MVC-benefits-background">
                     <div className="container">
