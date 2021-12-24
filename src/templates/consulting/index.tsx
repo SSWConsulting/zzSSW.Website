1315cb6207321a06f965117802bafbc2b23dd57f
pfmao <ErenJagerM@outlook.com>
2021-12-24 11:28:42 +0800

    refactor: fix some warnings

--- src/templates/consulting/index.tsx
+++ src/templates/consulting/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Layout from '../../components/layout';
 import Breadcrumb from '../../components/breadcrumb';
-import MVC_BACKGROUND from '../../assets/videos/MVC_background.mp4';
 import Booking from './components/booking';
 import Benefits from './components/benefits';
 import Testimonials from './components/testimonials';
@@ -12,7 +11,6 @@ import Technologies from './components/technologies';
 import Solution from './components/solution';
 
 import './index.module.css';
-import { backgroundVideo } from './index.module.css';
 
 const ConsultingTemplate = ({ data: { title } }) => {
     const data = [
