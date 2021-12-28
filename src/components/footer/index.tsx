824286d257ec9d70c692b09e2a3044fd0819e405
pfmao <ErenJagerM@outlook.com>
2021-12-28 17:58:46 +0800

    feat: adjust footer style

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,198 +1,94 @@
 import React from 'react';
-import preval from 'preval.macro';
-import moment from 'moment';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import {
-  faFacebook,
-  faGithub,
-  faInstagram,
-  faLinkedin,
-  faTwitter,
-  faWeixin,
-  faYoutube,
-} from '@fortawesome/free-brands-svg-icons';
-
-const buildTimestamp = preval`module.exports = new Date().getTime();`;
+import * as styles from './index.module.css';
 
 const Footer = () => {
-  return (
-    <>
-      <div className="py-2 text-center bg-grey-translucent text-sm" style={{ backgroundColor: 'white' }}>
-        <section className="main-container">
-          We <FontAwesomeIcon icon={faHeart} className="text-ssw-red" /> open
-          source. Powered by{' '}
-          <a
-            className="action-button-label footer-greybar-link"
-            href="https://github.com/SSWConsulting/SSW.Rules"
-          >
-            GitHub <FontAwesomeIcon icon={faGithub} />
-          </a>
-        </section>
-      </div>
-      <footer className="bg-black py-6 md:py-4 lg:py-2">
-        <section className="main-container">
-          <div className="xl:mx-6">
-            <div className="mx-6 flex flex-col-reverse md:flex-row justify-between align-middle leading-6">
-              <div className="py-2">
-                Copyright © SSW 1990 - {new Date().getFullYear()}. All Rights
-                Reserved.
-              </div>
-              <div className="w-full md:w-3/6 md:text-right py-2">
-                <a
-                  className="footer-link"
-                  href="https://github.com/SSWConsulting/SSW.Rules/issues"
-                >
-                  FEEDBACK / SUGGEST A FEATURE
-                </a>
-                <span className="px-2">|</span>
-                <a
-                  className="footer-link"
-                  href="https://www.ssw.com.au/ssw/Standards/Forms/ConsultingOrderTermsConditions.aspx"
-                >
-                  TERMS AND CONDITIONS
-                </a>
-                <span className="px-2">|</span>
-                <div className="inline-flex flex-row-reverse justify-end flex-nowrap">
-                  <a
-                    className="unstyled social-media-icon"
-                    id="youtube-icon"
-                    title="SSW on YouTube"
-                    href="https://www.youtube.com/user/sswtechtalks/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faYoutube} size="lg" />
-                  </a>
-                  <a
-                    className="unstyled social-media-icon"
-                    id="linkedin-icon"
-                    title="SSW on LinkedIn"
-                    href="https://www.linkedin.com/company/ssw/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faLinkedin} size="lg" />
-                  </a>
-                  <a
-                    className="unstyled social-media-icon"
-                    id="twitter-icon"
-                    title="SSW on Twitter"
-                    href="https://twitter.com/SSW_TV"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faTwitter} size="lg" />
-                  </a>
-                  <a
-                    className="unstyled social-media-icon"
-                    id="instagram-icon"
-                    title="SSW on Instagram"
-                    href="https://www.instagram.com/ssw_tv"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faInstagram} size="lg" />
-                  </a>
-                  <a
-                    className="unstyled social-media-icon"
-                    id="facebook-icon"
-                    title="SSW on Facebook"
-                    href="https://www.facebook.com/SSW.page"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faFacebook} size="lg" />
-                  </a>
-                  <a
-                    className="unstyled social-media-icon"
-                    id="wechat-icon"
-                    title="SSW on WeChat"
-                    href="https://mp.weixin.qq.com/s/jL4zEmzWM5VSTRu9DUW6-Q"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faWeixin} size="lg" />
-                    <span id="qr-code">
-                      <img
-                        src="http://www.ssw.com.au/ssw/Images/QRcode.jpg"
-                        alt="SSW QR Code"
-                        width="100"
-                        title="SSW WeChat QR Code"
-                        height="100"
-                      />
-                    </span>
-                  </a>
+    return (
+        <>
+            {/* TODO: Do we need this? */}
+            {/* <div
+                className="py-2 text-center bg-grey-translucent text-sm"
+                style={{ backgroundColor: 'white' }}
+            >
+                <section className="main-container">
+                    We{' '}
+                    <FontAwesomeIcon icon={faHeart} className="text-ssw-red" />{' '}
+                    open source. Powered by{' '}
+                    <a
+                        className="action-button-label footer-greybar-link"
+                        href="https://github.com/SSWConsulting/SSW.Rules"
+                    >
+                        GitHub <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                </section>
+            </div> */}
+            <footer className={styles.container}>
+                <div className="main-container">
+                    <div className={styles.infos}>
+                        <div>
+                            © Copyright SSW 1990-{new Date().getFullYear()}. All
+                            Rights Reserved.
+                        </div>
+                        <div>
+                            <a
+                                className={styles.link}
+                                href="https://github.com/SSWConsulting/SSW.Rules/issues"
+                            >
+                                FEEDBACK TO SSW
+                            </a>
+                            <span className={styles.divider}>|</span>
+                            <a
+                                className={styles.link}
+                                href="https://www.ssw.com.au/ssw/Standards/Forms/ConsultingOrderTermsConditions.aspx"
+                            >
+                                TERMS AND CONDITIONS
+                            </a>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className={styles.deployment}>
+                        Our website is under{' '}
+                        <a
+                            className={styles.link}
+                            href="https://www.ssw.com.au/rules/rules-to-better-websites-deployment"
+                        >
+                            CONSTANT CONTINUOUS DEPLOYMENT
+                        </a>
+                        . The latest deployment is listed{' '}
+                        <a
+                            className={styles.link}
+                            href="https://github.com/SSWConsulting/SSW.Website/actions/workflows/production-deploy-gatsby-site.yml"
+                        >
+                            HERE
+                        </a>
+                    </div>
                 </div>
-              </div>
-              {/* Copyright © SSW 1990 - {new Date().getFullYear()}. All Rights Reserved. */}
-            </div>
-            <hr className="border-gray-800 my-2"></hr>
-            <div className="flex flex-col md:flex-row justify-between mx-6">
-              <div className="py-2">
-                Our website is under{' '}
-                <a
-                  className="footer-link"
-                  href="https://www.ssw.com.au/rules/rules-to-better-websites-deployment"
-                >
-                  CONSTANT CONTINUOUS DEPLOYMENT
-                </a>
-                . Last deployed {getLastDeployTime()} ago (Build #{' '}
-                <a
-                  className="footer-link"
-                  href="https://github.com/SSWConsulting/SSW.Rules/releases/latest"
-                >
-                  {process.env.VERSION_DEPLOYED}
-                </a>
-                )
-              </div>
-              <div className="md:text-right py-2">
-                Powered by{' '}
-                <a
-                  className="footer-link"
-                  href="https://www.ssw.com.au/rules/rules-to-better-azure"
-                >
-                  Azure
-                </a>{' '}
-                and{' '}
-                <a
-                  className="footer-link"
-                  href="https://www.ssw.com.au/rules/static-site-generator"
-                >
-                  {' '}
-                  GitHub
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
-      </footer>
-    </>
-  );
+            </footer>
+        </>
+    );
 };
 
 //TODO: Fix getLastDeploymentTime
 const getLastDeployTime = () => {
-  // const lastDeployDuration = moment.duration(Date.now() - buildTimestamp);
-  // let delta = Math.abs(lastDeployDuration) / 1000;
+    // const lastDeployDuration = moment.duration(Date.now() - buildTimestamp);
+    // let delta = Math.abs(lastDeployDuration) / 1000;
 
-  // const days = Math.floor(delta / 86400);
-  // delta -= days * 86400;
+    // const days = Math.floor(delta / 86400);
+    // delta -= days * 86400;
 
-  // var hours = Math.floor(delta / 3600) % 24;
-  // delta -= hours * 3600;
+    // var hours = Math.floor(delta / 3600) % 24;
+    // delta -= hours * 3600;
 
-  // var minutes = Math.floor(delta / 60) % 60;
-  // delta -= minutes * 60;
+    // var minutes = Math.floor(delta / 60) % 60;
+    // delta -= minutes * 60;
 
-  // return days !== 0
-  //   ? `${days} day(s)`
-  //   : ' ' + hours !== 0
-  //   ? `${hours} hour(s)`
-  //   : ' ' + minutes > 1
-  //   ? `${minutes} minutes`
-  //   : '1 minute';
-  return `1 minute`;
+    // return days !== 0
+    //   ? `${days} day(s)`
+    //   : ' ' + hours !== 0
+    //   ? `${hours} hour(s)`
+    //   : ' ' + minutes > 1
+    //   ? `${minutes} minutes`
+    //   : '1 minute';
+    return `1 minute`;
 };
 
 Footer.propTypes = {};
