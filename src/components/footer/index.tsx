c164b71d46e472c4ccf59c72a43462d66550fa8a
pfmao <ErenJagerM@outlook.com>
2022-01-10 16:33:16 +0800

    feat: change layout structure and add semantic html to header and footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,82 +3,73 @@ import { StaticImage } from 'gatsby-plugin-image';
 import classNames from 'classnames';
 import * as styles from './index.module.css';
 
-const Footer = () => {
-    return (
-        <>
-            <footer className={styles.container}>
-                <div className="main-container">
-                    <div className={styles.infos}>
-                        <div>
-                            © Copyright SSW 1990-{new Date().getFullYear()}. All
-                            Rights Reserved.
-                        </div>
-                        <div>
-                            <a
-                                className={styles.link}
-                                href="https://github.com/SSWConsulting/SSW.Rules/issues/new/choose"
-                            >
-                                FEEDBACK TO SSW
-                            </a>
-                            <span className={styles.divider}>|</span>
-                            <a
-                                className={styles.link}
-                                href="https://www.ssw.com.au/ssw/Standards/Forms/ConsultingOrderTermsConditions.aspx"
-                            >
-                                TERMS AND CONDITIONS
-                            </a>
-                        </div>
-                    </div>
-                    <div
-                        className={classNames(
-                            'flex-between',
-                            styles.deployment
-                        )}
-                    >
-                        <div>
-                            Our website is under{' '}
-                            <a
-                                className={styles.link}
-                                href="https://www.ssw.com.au/rules/rules-to-better-websites-deployment"
-                            >
-                                CONSTANT CONTINUOUS DEPLOYMENT
-                            </a>
-                            . The latest deployment is listed{' '}
-                            <a
-                                className={styles.link}
-                                href="https://github.com/SSWConsulting/SSW.Website/actions/workflows/production-deploy-gatsby-site.yml"
-                            >
-                                HERE
-                            </a>
-                        </div>
-                        <div>
-                            <a
-                                className={styles.link}
-                                // TODO: Implementation
-                                href="https://github.com/SSWConsulting/SSW.Website/issues/63"
-                            >
-                                SITEMAP
-                            </a>
-                            <span className={styles.divider}>|</span>
-                            <a
-                                className={classNames(styles.link)}
-                                // TODO: Implementation
-                                href="https://github.com/SSWConsulting/SSW.Website/issues/64"
-                            >
-                                HEALTH CHECK{' '}
-                                <StaticImage
-                                    className={styles.healthCheck}
-                                    src="../../assets/images/health-check.png"
-                                    alt="health check "
-                                    layout="constrained"
-                                />
-                            </a>
-                        </div>
-                    </div>
+const Footer = () => (
+    <footer className={styles.container}>
+        <div className="main-container">
+            <div className={styles.infos}>
+                <div>
+                    © Copyright SSW 1990-{new Date().getFullYear()}. All Rights
+                    Reserved.
                 </div>
-            </footer>
-        </>
-    );
-};
+                <div>
+                    <a
+                        className={styles.link}
+                        href="https://github.com/SSWConsulting/SSW.Rules/issues/new/choose"
+                    >
+                        FEEDBACK TO SSW
+                    </a>
+                    <span className={styles.divider}>|</span>
+                    <a
+                        className={styles.link}
+                        href="https://www.ssw.com.au/ssw/Standards/Forms/ConsultingOrderTermsConditions.aspx"
+                    >
+                        TERMS AND CONDITIONS
+                    </a>
+                </div>
+            </div>
+            <div className={classNames('flex-between', styles.deployment)}>
+                <div>
+                    Our website is under{' '}
+                    <a
+                        className={styles.link}
+                        href="https://www.ssw.com.au/rules/rules-to-better-websites-deployment"
+                    >
+                        CONSTANT CONTINUOUS DEPLOYMENT
+                    </a>
+                    . The latest deployment is listed{' '}
+                    <a
+                        className={styles.link}
+                        href="https://github.com/SSWConsulting/SSW.Website/actions/workflows/production-deploy-gatsby-site.yml"
+                    >
+                        HERE
+                    </a>
+                </div>
+                <div>
+                    <a
+                        className={styles.link}
+                        // TODO: Implementation
+                        href="https://github.com/SSWConsulting/SSW.Website/issues/63"
+                    >
+                        SITEMAP
+                    </a>
+                    <span className={styles.divider}>|</span>
+                    <a
+                        className={classNames(styles.link)}
+                        // TODO: Implementation
+                        href="https://github.com/SSWConsulting/SSW.Website/issues/64"
+                    >
+                        HEALTH CHECK{' '}
+                        <StaticImage
+                            className={styles.healthCheck}
+                            src="../../assets/images/health-check.png"
+                            alt="health check "
+                            layout="constrained"
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+);
 
 export default Footer;
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,70 +1,18 @@
-import React, { useState, useRef } from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import classNames from 'classnames';
+import React from 'react';
 
 import Header from '../header';
-import Menu from '../../../lib/SSW.MegaMenu/menu/menu';
-import MobileMenu from '../../../lib/SSW.MegaMenu/mobile-menu/mobile-menu';
 import Footer from '../footer';
-
-// TODO: Keep it temporarily for reference
-// import '../style.css';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../../styles/global.css';
 import '../../styles/common.css';
 
-import { container } from './index.module.css';
-
-type LayoutProps = {
-    pageTitle: string;
-    children: JSX.Element;
-};
-
-const Layout = ({ pageTitle, children }: LayoutProps) => {
-    const node = useRef<HTMLDivElement>(null);
-    const [isMenuOpened, setIsMenuOpened] = useState(false);
-
-    const actionOnToggleClick = () => {
-        setIsMenuOpened(!isMenuOpened);
-    };
-
-    const handleClick = (e) => {
-        if (node.current.contains(e.target)) {
-            setIsMenuOpened(false);
-        }
-    };
-
-    const data = useStaticQuery(graphql`
-        query {
-            site {
-                siteMetadata {
-                    title
-                }
-            }
-        }
-    `);
-
+const Layout = ({ children }) => {
     return (
         <>
-            <div
-                ref={node}
-                onMouseDown={
-                    isMenuOpened ? (event) => handleClick(event) : null
-                }
-            >
-                <header className={container}>
-                    <div className="main-container">
-                        <Header />
-                        <Menu
-                            onClickToggle={() => actionOnToggleClick()}
-                        ></Menu>
-                    </div>
-                </header>
-                <main>{children}</main>
-                <Footer />
-            </div>
-            <MobileMenu isMenuOpened={isMenuOpened}></MobileMenu>
+            <Header />
+            <main>{children}</main>
+            <Footer />
         </>
     );
 };
