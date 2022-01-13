077877405bd816d574adb02822aabe55535133a2
pfmao <ErenJagerM@outlook.com>
2022-01-13 14:19:37 +0800

    feat: add image links

--- src/components/image-carousel/ssw-carousel.tsx
+++ src/components/image-carousel/ssw-carousel.tsx
@@ -1,76 +1,63 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import { StaticImage } from 'gatsby-plugin-image';
-import Modal from 'react-bootstrap/Modal';
 
 import './ssw-carousel.css';
 
 const { Item } = Carousel;
 
 const SSWCarousel = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
     //TODO: Look into looping through StaticImages, rather than hard coding
     return (
         <>
-            {/*TODO: Fix the video modal*/}
-            <Modal
-                size="lg"
-                show={isOpen}
-                dialogClassName="modal-80w"
-                onHide={() => {
-                    setIsOpen(false);
-                }}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>SSW Introduction Video</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <iframe
-                        src="https://player.vimeo.com/video/400817895?h=caf7b4856a&autoplay=1&color=cc4444&title=0&byline=0&portrait=0"
-                        width="640"
-                        height="360"
-                        allow="autoplay; fullscreen; picture-in-picture"
-                    ></iframe>
-                </Modal.Body>
-            </Modal>
-            <Carousel controls={false} indicators wrap touch>
+            <Carousel controls={false}>
                 <Item>
-                    <figure>
-                        <StaticImage
-                            src="../../assets/images/why-ssw.jpeg"
-                            alt="why ssw"
-                            onClick={() => {
-                                setIsOpen(true);
-                            }}
-                        />
-                    </figure>
+                    <a href="https://vimeo.com/400817895" target="_blank">
+                        <figure>
+                            <StaticImage
+                                src="../../assets/images/why-ssw.jpeg"
+                                alt="why ssw"
+                            />
+                        </figure>
+                    </a>
                 </Item>
                 <Item>
-                    <figure>
-                        <StaticImage
-                            src="../../assets/images/case-study.jpeg"
-                            alt="case study"
-                        />
-                    </figure>
+                    <a
+                        href="https://www.ssw.com.au/SSW/Consulting/Case-Study/SydneyUni.aspx"
+                        target="_blank"
+                    >
+                        <figure>
+                            <StaticImage
+                                src="../../assets/images/case-study.jpeg"
+                                alt="case study"
+                            />
+                        </figure>
+                    </a>
                 </Item>
                 <Item>
-                    <figure>
-                        <StaticImage
-                            src="../../assets/images/ssw-tv.png"
-                            alt="ssw tv"
-                            width={1140}
-                            height={410}
-                        />
-                    </figure>
+                    <a href="https://tv.ssw.com/" target="_blank">
+                        <figure>
+                            <StaticImage
+                                src="../../assets/images/ssw-tv.png"
+                                alt="ssw tv"
+                                width={1140}
+                                height={410}
+                            />
+                        </figure>
+                    </a>
                 </Item>
                 <Item>
-                    <figure>
-                        <StaticImage
-                            src="../../assets/images/need-help.jpeg"
-                            alt="need help?"
-                        />
-                    </figure>
+                    <a
+                        href="https://www.ssw.com.au/ssw/Consulting/Are-You-Stuck.aspx"
+                        target="_blank"
+                    >
+                        <figure>
+                            <StaticImage
+                                src="../../assets/images/need-help.jpeg"
+                                alt="need help?"
+                            />
+                        </figure>
+                    </a>
                 </Item>
             </Carousel>
         </>
