598cadf19accd6acaf621f86099ed8b9c5ef3b8a
Ozair Ashfaque [SSW] <96504411+ozairashfaqueSSW@users.noreply.github.com>
2022-01-25 17:30:41 +1100

    Refactor add consulting services panels component (#88)
    * Refector added_consulting_and_core_services_component
    
    * Refactor added css files for each component
    
    * refactor: formatting and moved components inside indexPage

--- src/pages/ssw/index.tsx
+++ src/pages/ssw/index.tsx
@@ -8,6 +8,8 @@ import Footer from '../../components/indexPage/footer';
 import OutlineButton from '../../components/outlineButton';
 
 import '../../styles/index.css';
+import ConsultingServices from '../../components/consultingServices';
+import CoreServices from '../../components/coreServices';
 
 // markup
 const IndexPage = () => {
@@ -17,97 +19,11 @@ const IndexPage = () => {
                 <Carousel />
 
                 {/* TODO: Extract to consulting component */}
-                <section id="whatwedo" className="main-container">
-                    <article className="row">
-                        <article className="col-sm-12">
-                            <h2 className="special">Consulting</h2>
-                        </article>
-                        <article className="col-md-3 col-sm-6 do-angular">
-                            {/* TODO: Update link after implement this page */}
-                            <a href="https://www.ssw.com.au/ssw/Consulting/Web-Applications.aspx">
-                                <h2>Web / Angular / React</h2>
-                                <p>
-                                    Ever since SSW developed the very first .NET
-                                    Website in Australia, we've been known as
-                                    leaders in Web Development.
-                                </p>
-                            </a>
-                        </article>
-                        <article className="col-md-3 col-sm-6 do-cloud">
-                            {/* TODO: Update link after implement this page */}
-                            <a href="https://www.ssw.com.au/ssw/Consulting/Azure.aspx">
-                                <h2>DevOps {'&'} Cloud</h2>
-                                <p>
-                                    Making your software ships by using SSW
-                                    developers' best practices in Azure, DevOps
-                                    and Docker Containers.
-                                </p>
-                            </a>
-                        </article>
-                        <article className="col-md-3 col-sm-6 do-mobile">
-                            {/* TODO: Update link after implement this page */}
-                            <a href="https://www.ssw.com.au/ssw/Consulting/Mobile-Application-Development.aspx">
-                                <h2>Mobile/Bots/AI</h2>
-                                <p>
-                                    SSW software developers are experts at
-                                    building cross platform mobile and chat apps
-                                    using the power of AI with Machine Learning.
-                                </p>
-                            </a>
-                        </article>
-
-                        <article className="col-md-3 col-sm-6 do-rules">
-                            {/* TODO: Update link after implement this page */}
-                            <a href="https://www.ssw.com.au/ssw/Consulting/Microsoft-Dynamics-365.aspx">
-                                <h2>Dynamics 365</h2>
-                                <p>
-                                    Dynamics 365/CRM brings new levels of
-                                    customer intelligence, providing employees
-                                    the information they need to truly impress.
-                                </p>
-                            </a>
-                        </article>
-                    </article>
-                </section>
-
+  
+                <ConsultingServices />
                 {/* TODO: Extract to services component */}
-                <section className="main-container services">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h2 className="special">Core Services</h2>
-                        </div>
-                        <div className="col-md-3 col-sm-6 core-training">
-                            {/* TODO: Update link after implement this page */}
-                            <a href="https://www.ssw.com.au/ssw/Events/?tech=all&type=all">
-                                <h3>Training</h3>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-sm-6 core-sharepoint">
-                            {/* TODO: Update link after implement this page */}
-                            <a href="https://www.ssw.com.au/ssw/Consulting/SharePoint.aspx">
-                                <h3>Office 365/SharePoint</h3>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-sm-6 core-book">
-                            {/* TODO: Update link after implement this page */}
-                            <a href="https://www.ssw.com.au/ssw/Consulting/Consulting.aspx">
-                                <h3>Book an Expert</h3>
-                            </a>
-                        </div>
-                        <div className="col-md-3 col-sm-6 core-crm">
-                            {/* TODO: Update link after implement this page */}
-                            <a
-                                href="https://www.ssw.com.au/rules/"
-                                target="_blank"
-                            >
-                                <h3>Rules</h3>
-                            </a>
-                        </div>
-                    </div>
-                    <OutlineButton href="https://www.ssw.com.au/ssw/Consulting/">
-                        More Services...
-                    </OutlineButton>
-                </section>
+                <CoreServices />
+
             </div>
 
             <AboutUsAndEvents />
