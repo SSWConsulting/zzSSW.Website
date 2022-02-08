5f226bca546e8de68df83d7dc922c9b31c3b9bcb
Ozair Ashfaque [SSW] <96504411+ozairashfaqueSSW@users.noreply.github.com>
2022-02-08 13:44:58 +1100

    feat: added zendesk chat plugin (#104)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,55 @@
-require('dotenv').config({
-    path: `.env.${process.env.NODE_ENV}`,
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
-    siteMetadata: {
-        siteUrl: 'https://www.yourdomain.tld',
-        title: 'SSW Website',
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "SSW Website",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-git`,
+      options: {
+        name: "content",
+        remote: process.env.CONTENT_REPO,
+        branch: process.env.CONTENT_BRANCH,
+        patterns: ["**/*.mdx", "**/*.png", "**/*.jpeg"],
+      },
     },
-    plugins: [
-        {
-            resolve: `gatsby-source-git`,
-            options: {
-                name: 'content',
-                remote: process.env.CONTENT_REPO,
-                branch: process.env.CONTENT_BRANCH,
-                patterns: ['**/*.mdx', '**/*.png', '**/*.jpeg'],
-            },
-        },
-        'gatsby-plugin-mdx',
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'pages',
-                path: './src/pages/',
-            },
-            __key: 'pages',
-        },
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'images',
-                path: './src/assets/images/',
-            },
-            __key: 'images',
-        },
-        'gatsby-plugin-image',
-        'gatsby-plugin-sharp',
-        'gatsby-transformer-sharp',
-        {
-            resolve: 'gatsby-plugin-google-tagmanager',
-            options: {
-                id: 'GTM-WP8CQLL',
-                includeInDevelopment: true,
-            },
-        },
-    ],
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/assets/images/",
+      },
+      __key: "images",
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-WP8CQLL",
+        includeInDevelopment: true,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-zendesk-chat",
+      options: {
+        zendeskKey: "4589cd27-2d80-457c-a420-621182f9c776",
+        enableDuringDevelop: true, // Optional. Disables Zendesk chat widget when running Gatsby dev server. Defaults to true.
+      },
+    },
+  ],
 };
