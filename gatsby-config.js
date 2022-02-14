a2f40207bc543fb7ef90720b0d7bcd2cd2e641eb
Luke <ErenJagerM@outlook.com>
2022-02-15 06:24:47 +0800

    feat: add sitemap (#107)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,55 +1,67 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
 });
 
+// TODO: Change Staging url to Prod url after deploying to Prod
+const siteUrl =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:9000'
+        : 'https://staging2.ssw.com.au';
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "SSW Website",
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-git`,
-      options: {
-        name: "content",
-        remote: process.env.CONTENT_REPO,
-        branch: process.env.CONTENT_BRANCH,
-        patterns: ["**/*.mdx", "**/*.png", "**/*.jpeg"],
-      },
+    siteMetadata: {
+        siteUrl: 'https://www.yourdomain.tld',
+        title: 'SSW Website',
     },
-    "gatsby-plugin-mdx",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/assets/images/",
-      },
-      __key: "images",
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-WP8CQLL",
-        includeInDevelopment: true,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-zendesk-chat",
-      options: {
-        zendeskKey: "4589cd27-2d80-457c-a420-621182f9c776",
-        enableDuringDevelop: true, // Optional. Disables Zendesk chat widget when running Gatsby dev server. Defaults to true.
-      },
-    },
-  ],
+    plugins: [
+        {
+            resolve: 'gatsby-source-git',
+            options: {
+                name: 'content',
+                remote: process.env.CONTENT_REPO,
+                branch: process.env.CONTENT_BRANCH,
+                patterns: ['**/*.mdx', '**/*.png', '**/*.jpeg'],
+            },
+        },
+        'gatsby-plugin-mdx',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/assets/images/',
+            },
+            __key: 'images',
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-plugin-google-tagmanager',
+            options: {
+                id: 'GTM-WP8CQLL',
+                includeInDevelopment: true,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-zendesk-chat',
+            options: {
+                zendeskKey: '4589cd27-2d80-457c-a420-621182f9c776',
+                enableDuringDevelop: true, // Optional. Disables Zendesk chat widget when running Gatsby dev server. Defaults to true.
+            },
+        },
+        {
+            resolve: `gatsby-plugin-sitemap`,
+            options: {
+                resolveSiteUrl: () => siteUrl,
+            },
+        },
+    ],
 };
