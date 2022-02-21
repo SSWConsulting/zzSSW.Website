7e211363ee8a5711d9a4e912562ea7f4f4b5a7ad
ozairashfaqueSSW <OzairAshfaque@ssw.com.au>
2022-02-21 20:30:34 +1100

    DevOps: Test 1

---
+++ .github/templates/test.ps1
@@ -0,0 +1,2 @@
+docker pull sswconsulting/codeauditor
+docker container run --rm --cap-add=SYS_ADMIN sswconsulting/codeauditor --url https://staging2.ssw.com.au/ssw/ --token 3c34a549-dfb3-442c-b0e3-45942104a8bf
