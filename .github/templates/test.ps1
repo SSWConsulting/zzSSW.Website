a182c110da94dc74078c9cbcbfcac2ab8a79af4e
ozairashfaqueSSW <OzairAshfaque@ssw.com.au>
2022-02-22 12:08:34 +1100

    DevOps: test codeauditor added

--- .github/templates/test.ps1
+++ .github/templates/test.ps1
@@ -1,2 +1,1 @@
-docker pull sswconsulting/codeauditor
-docker container run --rm --cap-add=SYS_ADMIN sswconsulting/codeauditor --url https://staging2.ssw.com.au/ssw/ --token 3c34a549-dfb3-442c-b0e3-45942104a8bf
+ docker run sswconsulting/codeauditor --token 3c34a549-dfb3-442c-b0e3-45942104a8bf --url https://staging2.ssw.com.au/ssw/ --maxthread 200 --debug
