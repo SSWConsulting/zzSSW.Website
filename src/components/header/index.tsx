5ee8d135c88d01c313f4c15ec0fecf0a6cf7f0c9
ozairashfaqueSSW <OzairAshfaque@ssw.com.au>
2022-02-28 10:52:11 +1100

    Feat: Added new megamenu and mobile menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import TopBar from './topBar';
-import Menu from '../../../lib/SSW.MegaMenu/menu/menu';
-import { container } from './index.module.css';
+import React, { useState, useRef } from "react";
+import TopBar from "./topBar";
+import Menu from "../../../lib/SSW.MegaMenu/menu/menu";
+import MobileMenu from "../../../lib/SSW.MegaMenu//mobile-menu/mobile-menu";
+import { container } from "./index.module.css";
 
-const Header = () => (
+const Header = () => {
+  const node = useRef(null);
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const actionOnToggleClick = () => {
+    setIsMenuOpened(!isMenuOpened);
+  };
+
+  const handleClick = (e) => {
+    console.log(node);
+    if (node.current.contains(e.target)) {
+      setIsMenuOpened(false);
+    }
+  };
+  return (
     <header className={container}>
-        <div className="main-container">
-            <TopBar />
-            <Menu />
-        </div>
+      <div className="main-container">
+        <TopBar />
+        <>
+          <div
+            ref={node}
+            className="App"
+            onMouseDown={isMenuOpened ? (event) => handleClick(event) : null}
+            style={{
+              transform: isMenuOpened ? "translateX(84%)" : "translateX(0px)",
+              zIndex: 2000,
+              position: "relative",
+            }}
+          >
+            <Menu onClickToggle={() => actionOnToggleClick()}></Menu>
+          </div>
+
+          <MobileMenu isMenuOpened={isMenuOpened}></MobileMenu>
+        </>
+      </div>
     </header>
-);
+  );
+};
 
 export default Header;
