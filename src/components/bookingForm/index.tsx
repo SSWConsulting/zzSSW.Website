9970988bd8431504d2b59d078074cec812d58bab
ozairashfaqueSSW <OzairAshfaque@ssw.com.au>
2022-04-05 16:03:40 +1000

    Added swtch cases and axios methods

--- src/components/bookingForm/index.tsx
+++ src/components/bookingForm/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Col, Form } from "react-bootstrap";
+import axios from "axios";
 import "./index.css";
 
 const BookingForm = () => {
@@ -7,8 +8,11 @@ const BookingForm = () => {
     FullName: "Full Name *",
     Email: "Email *",
     Phone: "Phone *",
-    Location: "Location *",
-    States: "States *",
+    Location: "Location",
+    States: "States",
+    Note: "Message",
+    Company: "Company",
+    ClassShow: "show",
     None: "",
   };
 
@@ -77,31 +81,87 @@ const BookingForm = () => {
 
   const [isShowState, setIsShowState] = useState(false);
   const [activeLabel, setActiveLabel] = useState({});
+  //Form Validation
+  const [validated, setValidated] = useState(false);
+  //Form Data
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [location, setLocation] = useState("");
+  const [state, setState] = useState("Other");
+  const [company, setCompany] = useState("");
+  const [note, setNote] = useState("");
 
-  const handleInputClicked = (targetInput, value) => {
+  const handleInput = (targetInput, value) => {
     if (
       (targetInput == ACTIVE_INPUT.FullName ||
         targetInput == ACTIVE_INPUT.Email ||
         targetInput == ACTIVE_INPUT.Phone ||
         targetInput == ACTIVE_INPUT.Location ||
-        targetInput == ACTIVE_INPUT.States) &&
+        targetInput == ACTIVE_INPUT.Company ||
+        targetInput == ACTIVE_INPUT.Note) &&
       value.trim().length > 0
     ) {
       setActiveLabel({ ...activeLabel, [targetInput]: true });
+      switch (targetInput) {
+        case ACTIVE_INPUT.FullName:
+          setFullName(value);
+        case ACTIVE_INPUT.Email:
+          setEmail(value);
+        case ACTIVE_INPUT.Phone:
+          setPhone(value);
+        case ACTIVE_INPUT.Location:
+          if (state.trim().length > 0) {
+            setState("Other");
+          }
+          setLocation(value);
+        case ACTIVE_INPUT.Company:
+          setCompany(value);
+        case ACTIVE_INPUT.Note:
+          setNote(value);
+        default:
+          break;
+      }
+    } else if (targetInput == ACTIVE_INPUT.States && location == "australia") {
+      setActiveLabel({ ...activeLabel, [targetInput]: true });
+      setState(value);
     } else {
       setActiveLabel({ ...activeLabel, [targetInput]: false });
     }
   };
 
-  const [validated, setValidated] = useState(false);
   const handleSubmit = (event) => {
+    event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
-      event.preventDefault();
       event.stopPropagation();
+    } else {
+      setValidated(true);
+      const subject = "Consulting enquiry - " + company + " - " + fullName;
+      let body = "Consulting enquiry from " + document.URL + "<br/>";
+      body = body + "Company: " + company + "<br/>";
+      body = body + "State:  " + state + "<br/>";
+      body = body + "Name:  " + fullName + "<br/>";
+      body = body + "Phone:   " + phone + "<br/>";
+      body = body + "Email:   " + email + "<br/>";
+      body = body + "Note:    " + note + "<br/><br/>";
+      const data = {
+        Name: fullName,
+        Topic: subject,
+        Company: company,
+        Note: note,
+        Country: location,
+        State: state,
+        Email: email,
+        Phone: phone,
+        EmailSubject: subject,
+        EmailBody: body + "The associated CRM lead is ",
+      };
+      axios
+        .post("https://www.ssw.com.au/ssw/api/crm/createlead", data)
+        .then((response) => console.log(response))
+        .catch((err) => console.log(err));
     }
-
-    setValidated(true);
   };
 
   let theStateList;
@@ -111,14 +171,18 @@ const BookingForm = () => {
         <Form.Group controlId="australianStateSelect">
           <div className="field-wrapper">
             <Form.Label
-              className={activeLabel[ACTIVE_INPUT.States] == true ? "show" : ""}
+              className={
+                activeLabel[ACTIVE_INPUT.States] == true
+                  ? ACTIVE_INPUT.ClassShow
+                  : ACTIVE_INPUT.None
+              }
             >
               {ACTIVE_INPUT.States}
             </Form.Label>
             <Form.Select
               required
               onClick={(e) => {
-                handleInputClicked(ACTIVE_INPUT.States, e.currentTarget.value);
+                handleInput(ACTIVE_INPUT.States, e.currentTarget.value);
               }}
             >
               <option className="d-none" value="">
@@ -153,23 +217,25 @@ const BookingForm = () => {
               <div className="field-wrapper">
                 <Form.Label
                   className={
-                    activeLabel[ACTIVE_INPUT.FullName] == true ? "show" : ""
+                    activeLabel[ACTIVE_INPUT.FullName] == true
+                      ? ACTIVE_INPUT.ClassShow
+                      : ACTIVE_INPUT.None
                   }
                 >
                   {ACTIVE_INPUT.FullName}
                 </Form.Label>
                 <Form.Control
                   onChange={(e) => {
-                    handleInputClicked(
-                      ACTIVE_INPUT.FullName,
-                      e.currentTarget.value
-                    );
+                    handleInput(ACTIVE_INPUT.FullName, e.currentTarget.value);
                   }}
                   required
                   type="text"
-                  placeholder="Full Name *"
+                  placeholder={ACTIVE_INPUT.FullName}
                 />
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please provide your full name.
+                </Form.Control.Feedback>
               </div>
             </Form.Group>
             <Form.Group as={Col} controlId="validationEmail">
@@ -183,14 +249,11 @@ const BookingForm = () => {
                 </Form.Label>
                 <Form.Control
                   onChange={(e) => {
-                    handleInputClicked(
-                      ACTIVE_INPUT.Email,
-                      e.currentTarget.value
-                    );
+                    handleInput(ACTIVE_INPUT.Email, e.currentTarget.value);
                   }}
                   required
                   type="email"
-                  placeholder="Email *"
+                  placeholder={ACTIVE_INPUT.Email}
                 />
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 <Form.Control.Feedback type="invalid">
@@ -202,20 +265,19 @@ const BookingForm = () => {
               <div className="field-wrapper">
                 <Form.Label
                   className={
-                    activeLabel[ACTIVE_INPUT.Phone] == true ? "show" : ""
+                    activeLabel[ACTIVE_INPUT.Phone] == true
+                      ? ACTIVE_INPUT.ClassShow
+                      : ACTIVE_INPUT.None
                   }
                 >
                   {ACTIVE_INPUT.Phone}
                 </Form.Label>
                 <Form.Control
                   onChange={(e) => {
-                    handleInputClicked(
-                      ACTIVE_INPUT.Phone,
-                      e.currentTarget.value
-                    );
+                    handleInput(ACTIVE_INPUT.Phone, e.currentTarget.value);
                   }}
                   type="phone"
-                  placeholder="Phone "
+                  placeholder={ACTIVE_INPUT.Phone}
                 />
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               </div>
@@ -224,7 +286,9 @@ const BookingForm = () => {
               <div className="field-wrapper">
                 <Form.Label
                   className={
-                    activeLabel[ACTIVE_INPUT.Location] == true ? "show" : ""
+                    activeLabel[ACTIVE_INPUT.Location] == true
+                      ? ACTIVE_INPUT.ClassShow
+                      : ACTIVE_INPUT.None
                   }
                 >
                   {ACTIVE_INPUT.Location}
@@ -232,10 +296,7 @@ const BookingForm = () => {
                 <Form.Select
                   required
                   onClick={(e) => {
-                    handleInputClicked(
-                      ACTIVE_INPUT.Location,
-                      e.currentTarget.value
-                    );
+                    handleInput(ACTIVE_INPUT.Location, e.currentTarget.value);
                   }}
                   onChange={(e) => {
                     e.currentTarget.value == "australia"
@@ -255,8 +316,43 @@ const BookingForm = () => {
               </div>
             </Form.Group>
             {theStateList}
+            <Form.Group as={Col} controlId="validationCompany">
+              <div className="field-wrapper">
+                <Form.Label
+                  className={
+                    activeLabel[ACTIVE_INPUT.Company] == true
+                      ? ACTIVE_INPUT.ClassShow
+                      : ACTIVE_INPUT.None
+                  }
+                >
+                  {ACTIVE_INPUT.Company}
+                </Form.Label>
+                <Form.Control
+                  onChange={(e) => {
+                    handleInput(ACTIVE_INPUT.Company, e.currentTarget.value);
+                  }}
+                  required
+                  type="text"
+                  placeholder={ACTIVE_INPUT.Company}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                  Please provide your full name.
+                </Form.Control.Feedback>
+              </div>
+            </Form.Group>
             <Form.Group controlId="contactFormNote">
               <div className="field-wrapper">
+                <Form.Label
+                  className={
+                    activeLabel[ACTIVE_INPUT.Note] == true
+                      ? ACTIVE_INPUT.ClassShow
+                      : ACTIVE_INPUT.None
+                  }
+                >
+                  {ACTIVE_INPUT.Note}
+                </Form.Label>
+
                 <Form.Control
                   as="textarea"
                   placeholder="Note"
@@ -264,6 +360,9 @@ const BookingForm = () => {
                   maxLength={2000}
                   name="address"
                   required
+                  onChange={(e) => {
+                    handleInput(ACTIVE_INPUT.Note, e.currentTarget.value);
+                  }}
                 />
                 <small>Maximium 2000 characters</small>
                 <Form.Control.Feedback type="invalid">
