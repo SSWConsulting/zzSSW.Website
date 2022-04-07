d25bcfedfdaf2afd19ac8df0dd5c52efbeb27677
Luke Cook <LukeCook@ssw.com.au>
2022-04-07 13:57:34 +1000

    changed template to use MDXRenderer for markdown content. Added data-driven other technology. Created technology components

--- src/pages/ssw/{mdx.slug}.tsx
+++ src/pages/ssw/{mdx.slug}.tsx
@@ -5,15 +5,10 @@ import Consulting from '../../templates/consulting';
 import NotFound from '../404';
 import { PAGE_TYPE } from '../../consts';
 
-const Page = ({
-    data: {
-        mdx: { frontmatter },
-    },
-}) => {
-    const { type } = frontmatter;
-
+const Page = ({data}) => {
+    const { type } = data.mdx.frontmatter;
     if (type === PAGE_TYPE.Consulting) {
-        return <Consulting data={frontmatter} />;
+        return <Consulting data={data} />;
     }
 
     return <NotFound />;
@@ -57,7 +52,11 @@ export const query = graphql`
                 solution {
                     project
                 }
+                technologies {
+                    name
+                }
             }
+            body
         }
     }
 `;
