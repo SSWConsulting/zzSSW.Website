a5ae8347875a391fd4449a0defbb20ba85e1d498
Ozair Ashfaque [SSW] <96504411+ozairashfaqueSSW@users.noreply.github.com>
2022-04-12 11:27:28 +1000

    Feat add booking form (#144)
    * Feat: Add Booking form
    
    * Added form properties
    
    * added_react_modal_for_contactUs_form
    
    * added label logic and fixed Button component
    
    * Added swtch cases and axios methods

--- src/templates/consulting/components/booking/index.tsx
+++ src/templates/consulting/components/booking/index.tsx
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import VideoBackground from '../../../../components/videoBackground';
-import InnerHTML from '../../../../components/innerHTML';
-import Button from '../../../../components/button';
+import VideoBackground from "../../../../components/videoBackground";
+import InnerHTML from "../../../../components/innerHTML";
+import Button from "../../../../components/button";
 
-import MVC_BACKGROUND from '../../../../assets/videos/MVC_background.mp4';
-import * as styles from './index.module.css';
+import MVC_BACKGROUND from "../../../../assets/videos/MVC_background.mp4";
+import * as styles from "./index.module.css";
+import BookingFormPopup from "../../../../components/bookingFormPopup";
 
-const Booking = ({ title, subTitle }) => (
+const Booking = ({ title, subTitle }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const showBookingForm = () => setIsVisible(!isVisible);
+
+  return (
     <>
-        <VideoBackground src={MVC_BACKGROUND} />
-        <section className={styles.container}>
-            <article className="main-container">
-                <InnerHTML tagName="h1" __html={title} />
+      <VideoBackground src={MVC_BACKGROUND} />
+      <section className={styles.container}>
+        <article className="main-container">
+          <InnerHTML tagName="h1" __html={title} />
 
-                <h2 className={styles.subTitle}>{subTitle}</h2>
+          <h2 className={styles.subTitle}>{subTitle}</h2>
 
-                <Button>Book a FREE Initial Meeting</Button>
+          <Button onClick={showBookingForm}>Book a FREE Initial Meeting</Button>
+          <BookingFormPopup
+            isVisible={isVisible}
+            showBookingForm={setIsVisible}
+          />
 
-                <h2 className={styles.contact}>
-                    or call us on +61 2 9953 3000
-                </h2>
+          <h2 className={styles.contact}>or call us on +61 2 9953 3000</h2>
 
-                <div className={`${styles.arrowScroll} ${styles.bounce}`}>
-                    <a
-                        className={`${styles.fa} ${styles.faChevronDown}`}
-                        href="#more"
-                    ></a>
-                </div>
-            </article>
-        </section>
+          <div className={`${styles.arrowScroll} ${styles.bounce}`}>
+            <a
+              className={`${styles.fa} ${styles.faChevronDown}`}
+              href="#more"
+            ></a>
+          </div>
+        </article>
+      </section>
     </>
-);
+  );
+};
 
 export default Booking;
