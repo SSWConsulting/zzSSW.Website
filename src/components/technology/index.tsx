c0a0b6ecc58ea13e257fd260f427a7569387c873
Ozair Ashfaque [SSW] <96504411+ozairashfaqueSSW@users.noreply.github.com>
2022-04-22 15:21:10 +1000

    Feat add technologies component (#153)
    * Refactor consulting pages using gatsby-node
    
    * Feat: added technology component
    
    * Feat added last child col class control and deleted unnecassry code
    
    * refactor: deleting mdx.slug file and replacing the path
    
    * refactor: merged main

---
+++ src/components/technology/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
+import { skill } from "./index.module.css";
+import { Link } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { BASE_URL } from "../../constants";
+
+const Technology = (props) => {
+  const { techListLength, frontmatter, body, index } = props;
+  const { title, logoImage, readMoreSlug } = frontmatter;
+  let theReadMoreLink;
+  let columnClass;
+  if (techListLength % 2 != 0 && techListLength - 1 == index) {
+    columnClass = "col-md-12";
+  } else {
+    columnClass = "col-md-6";
+  }
+  if (readMoreSlug) {
+    const link = BASE_URL + readMoreSlug;
+    theReadMoreLink = (
+      <>
+        <Link to={link}>Read More</Link>
+      </>
+    );
+  }
+
+  return (
+    <div className={columnClass}>
+      <article className={skill} data-aos="flip-left">
+        <figure>
+          <GatsbyImage image={getImage(logoImage)} alt={title} />
+        </figure>
+        <MDXRenderer>{body}</MDXRenderer>
+        {theReadMoreLink}
+      </article>
+    </div>
+  );
+};
+
+export default Technology;
