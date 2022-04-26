57a5fdf613d97d861df2ce9e11ee67a717cc7530
ozairashfaqueSSW <OzairAshfaque@ssw.com.au>
2022-04-26 14:57:18 +1000

    bug fixed : video overflow

--- src/templates/consulting/components/benefits/index.tsx
+++ src/templates/consulting/components/benefits/index.tsx
@@ -1,61 +1,51 @@
-import React, { useState } from 'react';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-
-import InnerHTML from '../../../../components/innerHTML';
+import React, { useState } from "react";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import "./index.css";
 
 const Benefits = ({ benefits, markdownContent }) => {
-    const [videoClicked, setVideoClicked] = useState(false);
-    const { title, video, descriptions, benefitList, rule } = benefits;
-    const videoId = video?.url?.split('v=')[1] ?? '';
+  const [videoClicked, setVideoClicked] = useState(false);
+  const { title, video, descriptions, benefitList, rule } = benefits;
+  const videoId = video?.url?.split("v=")[1] ?? "";
 
-    return (
-        <section className="container">
-            <a id="more" />
-            <article className="main-container">
-                <MDXRenderer>
-                    {markdownContent}
-                </MDXRenderer>
+  return (
+    <section className="container">
+      <a id="more" />
+      <article className="main-container">
+        <MDXRenderer>{markdownContent}</MDXRenderer>
 
-                <div className="flex-wrap">
-                    {benefitList?.map(
-                        ({ image, title, description }, index) => (
-                            <article
-                                className={`col-md-6 benefit`}
-                                key={index}
-                                data-aos={
-                                    index % 2 === 0 ? 'fade-left' : 'fade-right'
-                                }
-                            >
-                                <figure>
-                                    <GatsbyImage
-                                        className="fl"
-                                        image={getImage(image)}
-                                        alt="benefit icon"
-                                    />
-                                </figure>
-                                <h4 className="benefitTitle">{title}</h4>
-                                <article className="benefitDesc">
-                                    {description}
-                                </article>
-                            </article>
-                        )
-                    )}
-                </div>
-
-                {rule && (
-                    <p>
-                        Have a look at{' '}
-                        <a className="link" href={rule?.url}>
-                            {rule?.name}
-                        </a>
-                        .
-                    </p>
-                )}
+        <div className="flex-wrap">
+          {benefitList?.map(({ image, title, description }, index) => (
+            <article
+              className={`col-md-6 benefit`}
+              key={index}
+              data-aos={index % 2 === 0 ? "fade-left" : "fade-right"}
+            >
+              <figure>
+                <GatsbyImage
+                  className="fl"
+                  image={getImage(image)}
+                  alt="benefit icon"
+                />
+              </figure>
+              <h4 className="benefitTitle">{title}</h4>
+              <article className="benefitDesc">{description}</article>
             </article>
-        </section>
-    );
+          ))}
+        </div>
+
+        {rule && (
+          <p>
+            Have a look at{" "}
+            <a className="link" href={rule?.url}>
+              {rule?.name}
+            </a>
+            .
+          </p>
+        )}
+      </article>
+    </section>
+  );
 };
 
 export default Benefits;
