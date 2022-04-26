7a28fd896d13c5aa954b9a86ce2d792af45b2bd2
Kevin Pho <101692458+kevinpho2022@users.noreply.github.com>
2022-04-26 16:54:28 +1000

    Delete index.tsx
    deleted files

--- src/pages/ssw/consulting/are-you-stuck/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import Testimonials from "../../../../templates/consulting/components/testimonials";
-
-export default function Component () {
-  return (
-    <Testimonials />
-  )
-}
