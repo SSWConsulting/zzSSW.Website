15aa83ffec6c2ac0253932849d234649949a33c3
KevinPho <KevinPho@ssw.com.au>
2022-04-29 10:06:55 +1000

    added imports added breadcrumb data added hard coded components

--- src/pages/ssw/consulting/are-you-stuck/index.tsx
+++ src/pages/ssw/consulting/are-you-stuck/index.tsx
@@ -1,8 +1,47 @@
-import * as React from "react";
-import Testimonials from "../../../../templates/consulting/components/testimonials";
+import React from "react";
 
-export default function Component () {
+import Breadcrumb from './../../../../components/breadcrumb';
+import Clients from "../../../../components/clients";
+import Layout from "../../../../components/layout";
+import About from "../../../../templates/consulting/components/about";
+import Testimonials from "../../../../templates/consulting/components/testimonials";
+import Solution from "../../../../templates/consulting/components/solution";
+import BuildPlatform from './../../../../components/builtPlatform';
+
+
+const AreYouStuck = ({ data }) => {
+  const { title, solution } = data
+  const pageTitle = `${title} | SSW Consulting - Sydney, Brisbane, Melbourne`
+  const breadcrumbData = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'Services',
+        path: '/consulting',
+    },
+    {
+        name: pageTitle,
+    },
+];
+
+
+  
+
   return (
-    <Testimonials />
-  )
-}
+    <Layout pageTitle={pageTitle}>
+      <>
+        <Breadcrumb data={breadcrumbData}/>
+
+        <Testimonials />
+
+        <About />
+
+        <Clients />
+
+      </>
+
+    </Layout>
+  );
+};
