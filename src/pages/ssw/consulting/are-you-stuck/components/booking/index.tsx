b041af4a5acd7b47ff3bf3fa95758b029c23cf55
KevinPho <KevinPho@ssw.com.au>
2022-05-03 14:59:42 +1000

    Added minor styling and video + heading

--- src/pages/ssw/consulting/are-you-stuck/components/booking/index.tsx
+++ src/pages/ssw/consulting/are-you-stuck/components/booking/index.tsx
@@ -21,6 +21,14 @@ const Booking = ({ title, subTitle }) => {
 
           <h2 className={styles.subTitle}>{subTitle}</h2>
 
+          <ul>
+            <li>We can help you adopt best practices</li>
+            <li>We can upskill you on the latest technologies</li>
+            <li>We can add weight and leverage to the decisions that you already know need to happen</li>
+          </ul>
+          
+          <h5>Share this page with your boss and get 50% OFF on your next SuperPowers ticket!</h5>
+
           <Button onClick={showBookingForm}>Refer SSW Consulting Services</Button>
           <BookingFormPopup
             isVisible={isVisible}
@@ -29,13 +37,19 @@ const Booking = ({ title, subTitle }) => {
 
           <h2 className={styles.contact}>or call us on +61 2 9953 3000</h2>
 
-          <ul>
-            <li>We can help you adopt best practices</li>
-            <li>We can upskill you on the latest technologies</li>
-            <li>We can add weight and leverage to the decisions that you already know need to happen</li>
-          </ul>
-          
-          <h5>Share this page with your boss and get 50% OFF on your next SuperPowers ticket!</h5>
+          <div>
+            <iframe
+              width="560" height="315"
+              src="https://www.youtube.com/embed/esPfjax-G-I"
+              frameBorder="0"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+              title="video"
+            />{" "}
+          </div>
+
+          <h5>Once your application has been built to run in a container, it can be easily hosted with any provider that supports containers, including Kubernetes and Azure Web Apps.</h5>
+
         </article>
       </section>
     </>
