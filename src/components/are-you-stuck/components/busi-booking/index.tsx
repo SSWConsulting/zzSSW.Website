002aae8842c8179e48eea792272a887daaa3f61a
KevinPho <KevinPho@ssw.com.au>
2022-05-06 12:32:51 +1000

    Initial commit - fixed imports

---
+++ src/components/are-you-stuck/components/busi-booking/index.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+import VideoBackground from "../../../../../../components/videoBackground";
+import InnerHTML from "../../../../../../components/innerHTML";
+import Button from "../../../../../../components/button";
+import ToggleButton from "../toggle-button"
+// this is fine
+import MVC_BACKGROUND from "../../../../../../assets/videos/MVC_background.mp4"
+import * as styles from "./index.module.css";
+import BookingFormPopup from "../../../../../../components/bookingFormPopup";
+
+const BusiBooking = ({ title, subTitle }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const showBookingForm = () => setIsVisible(!isVisible);
+
+  const [busiBooking, devBooking] = useState(true);
+  const bookingToggle = () => devBooking(!busiBooking);
+
+  return (
+    <>
+      <VideoBackground src={MVC_BACKGROUND} />
+      <section className={styles.container}>
+        <article className="main-container">
+          <InnerHTML tagName="h1" __html={title} />
+
+          <h2 className={styles.subTitle}>{subTitle}</h2>
+          {/* make copy of Button from the above directory to use here */}
+          <div>
+            <Button onClick={""}>I'm a Business Person</Button>
+            <Button onClick={""}>I'm a Developer</Button>
+          </div>
+
+          <h5>We are enterprise software consultants who solve complex business problems and reduce risk, using best practices and the latest technologies.</h5>
+          <h5>See how SSW can help:</h5>
+
+          <ul>
+            <li>Build it right the 1st time</li>
+            <li>Reduce risk through transparency, communication and fast feedback loops</li>
+            <li>High value, quality &amp; range of expertise</li>
+            <li>We are Microsoft Gold Certified partner, we have three MVPs and a Microsoft Regional Director</li>
+          </ul>
+          
+          <Button onClick={showBookingForm}>Book a Free Initial Meeting</Button>
+          <BookingFormPopup
+            isVisible={isVisible}
+            showBookingForm={setIsVisible}
+          />
+
+          <div>
+            <iframe
+              width="560" height="315"
+              src="https://www.youtube.com/embed/wiFadV5SOPc"
+              frameBorder="0"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+              title="video"
+              className="videoContainer"
+            />
+          </div>
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default BusiBooking;
---
+++ src/components/are-you-stuck/components/dev-booking/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+import VideoBackground from "../../../../../../components/videoBackground";
+import InnerHTML from "../../../../../../components/innerHTML";
+import Button from "../../../../../../components/button";
+import ToggleButton from "../toggle-button"
+// this is fine
+import MVC_BACKGROUND from "../../../../../../assets/videos/MVC_background.mp4"
+import * as styles from "./index.module.css";
+import BookingFormPopup from "../../../../../../components/bookingFormPopup";
+
+const Booking = ({ title, subTitle }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const showBookingForm = () => setIsVisible(!isVisible);
+
+  const [busiBooking, devBooking] = useState(true);
+  const bookingToggle = () => devBooking(!busiBooking);
+
+  return (
+    <>
+      <VideoBackground src={MVC_BACKGROUND} />
+      <section className={styles.container}>
+        <article className="main-container">
+          <InnerHTML tagName="h1" __html={title} />
+
+          <h2 className={styles.subTitle}>{subTitle}</h2>
+          {/* make copy of Button from the above directory to use here */}
+          <div>
+            <Button onClick={""}>I'm a Business Person</Button>
+            <Button onClick={""}>I'm a Developer</Button>
+          </div>
+
+          
+
+          <ul>
+            <li>We can help you adopt best practices</li>
+            <li>We can upskill you on the latest technologies</li>
+            <li>We can add weight and leverage to the decisions that you already know need to happen</li>
+          </ul>
+          
+          <h5>Share this page with your boss and get 50% OFF on your next SuperPowers ticket!</h5>
+
+          <Button onClick={showBookingForm}>Refer SSW Consulting Services</Button>
+          <BookingFormPopup
+            isVisible={isVisible}
+            showBookingForm={setIsVisible}
+          />
+
+          <div>
+            <iframe
+              width="560" height="315"
+              src="https://www.youtube.com/embed/esPfjax-G-I"
+              frameBorder="0"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+              title="video"
+              className="videoContainer"
+            />
+          </div>
+
+          <h5 className="videoText">Once your application has been built to run in a container, it can be easily hosted with any provider that supports containers, including Kubernetes and Azure Web Apps.</h5>
+
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default Booking;
