e0751be05ac2c8bedef0ec42f4faa0af3b623cc6
Anthony Doueal <doueal.a@gmail.com>
2022-05-13 17:22:20 +1000

    add mdx frontmatter and body to the legacy page content

--- src/templates/legacy/index.tsx
+++ src/templates/legacy/index.tsx
@@ -3,15 +3,15 @@ import React from "react"
 import Layout from "../../components/layout";
 import Breadcrumb from "../../components/breadcrumb";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 import * as styles from "./index.module.css";
 
 import speech_bubble from '../../assets/images/speech-bubble.png';
 
 const LegacyTemplate = ({ data }) => {
-    const content = data;
-    console.log(content.allMdx.nodes);
-    const pageTitle = `Software Auditing | SSW Consulting - Sydney, Brisbane, Melbourne`;
+    const { title, subtitle } = data.mdx.frontmatter
+    const pageTitle = `${title} | SSW Consulting - Sydney, Brisbane, Melbourne`;
     const breadcrumbData = [
         {
           name: "Home",
@@ -27,30 +27,20 @@ const LegacyTemplate = ({ data }) => {
     ];
 
     return (
-        <Layout pageTitle="Software Auditing | SSW Consulting - Sydney, Brisbane, Melbourne">
+        <Layout pageTitle={pageTitle}>
             <div className='legacy-banner'>
                 <img src={speech_bubble} />
                 <Breadcrumb data={breadcrumbData} />
-         
-                <h1 className='no-header-margin'>Software Auditing</h1>
-                <h2>Auditing services to improve your software</h2>
+                <h1 className='no-header-margin'>{title}</h1>
+                <h2>{subtitle}</h2>
             </div>
             <div className="main-container">
-            {/* { 
-                content.allMdx.nodes.map((node) => (
-                    <article key={node.id}>
-                        <h2>{node.frontmatter.title}</h2>
-                    </article>
-                ))
-            } */}
                 <div className="row">
                     <div className="col-9">
                         <div className="body">
-                            <h2><a id="overview"></a>Fine tune your software with SSW software auditing services</h2>
-                            <p>A crisis will pinpoint the shortfalls of your current technologies. Why wait for down-time or poor sales to discover where your application could be improved?</p>
-                            <p>The companies that we have done audits for range from developer utilities to major enterprise systems.</p>
-                            <p>Read <a href="https://www.ssw.com.au/rules/mentoring-programs">Do you have a mentoring program?</a></p>
-                            <h3>How long does this normally take?</h3>
+                            <MDXRenderer>
+                                {data.mdx.body}
+                            </MDXRenderer>
                         </div>
                     </div>
                     <div className="col-3">
@@ -76,16 +66,14 @@ const LegacyTemplate = ({ data }) => {
     );
 }
 export const legacyPageQuery = graphql`
-    query legacyContentQuery {
-        allMdx(filter: {fields: {source: {eq: "legacy"}}}) {
-        nodes {
-            slug
-            body
+    query legacyPage($slug: String) {
+        mdx(slug: { eq: $slug }) {
             frontmatter {
-            title
-            type
+                type
+                title
+                subtitle
             }
-        }
+            body
         }
     }
 `;
