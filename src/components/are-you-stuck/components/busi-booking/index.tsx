b38fc777cf8ffe3a47abf3f9b5cbe54f63dec008
KevinPho <KevinPho@ssw.com.au>
2022-05-17 18:02:09 +1000

    fixed up video container css

--- src/components/are-you-stuck/components/busi-booking/index.tsx
+++ src/components/are-you-stuck/components/busi-booking/index.tsx
@@ -12,11 +12,11 @@ const BusiBooking = () => {
       <section>
         <h5 className={styles.firstLine}>We are enterprise software consultants who solve complex business problems and reduce risk, using best practices and the latest technologies.</h5>
         <h5 className={styles.firstLine}>See how SSW can help:</h5>
-        <ul>
-          <li>Build it right the 1st time</li>
-          <li>Reduce risk through transparency, communication and fast feedback loops</li>
-          <li>High value, quality &amp; range of expertise</li>
-          <li>We are Microsoft Gold Certified partner, we have three MVPs and a Microsoft Regional Director</li>
+        <ul >
+          <li className={styles.theList}>Build it right the 1st time</li>
+          <li className={styles.theList}>Reduce risk through transparency, communication and fast feedback loops</li>
+          <li className={styles.theList}>High value, quality &amp; range of expertise</li>
+          <li className={styles.lastItem}>We are Microsoft Gold Certified partner, we have three MVPs and a Microsoft Regional Director</li>
         </ul>
         
         <Button onClick={showBookingForm}>Book a Free Initial Meeting</Button>
@@ -33,7 +33,7 @@ const BusiBooking = () => {
             allow="autoplay; encrypted-media"
             allowFullScreen
             title="video"
-            className="videoContainer"
+            className={styles.videoContainer}
           />
         </div>
       </section>
