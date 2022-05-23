a4b7f9d80dc508d1d543f7d5a7f8458ff833c1c5
ozairashfaqueSSW <OzairAshfaque@ssw.com.au>
2022-05-23 16:53:21 +1000

    feat added thankyou page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,21 @@
 export const PAGE_TYPE = {
+  Home: "Home",
   Consulting: "consulting",
+  Thankyou: "Thank you for contacting SSW!",
 };
+
+//Page Types for BreadCrumbs data
 export const BASE_URL = "/";
+
+//Page Titles for layout component
+export const PAGE_TITLE = {
+  Consulting:
+    "SSW Consulting - .NET, Web, Mobile, CRM, SharePoint, Azure, Power BI, Angular, React, Office 365 and Dynamics",
+
+  Thankyou: "Thank you for consulting SSW!",
+};
+
+//Images Import
+export const IMAGES_LINKS = {
+  Azure: require("../assets/images/bg.jpg"),
+};
