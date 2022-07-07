481b5b147258fac52ed580f65f2a5162cc2e6e9a
ozairashfaqueSSW <OzairAshfaque@ssw.com.au>
2022-07-07 11:46:33 +1000

    Added assets source, imageWithFallBack component, and updated technology and constants

--- src/components/technology/index.tsx
+++ src/components/technology/index.tsx
@@ -3,11 +3,14 @@ import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 import { skill } from "./index.module.css";
 import { Link } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { BASE_URL } from "../../constants";
+import { BASE_IMAGE_URL, BASE_URL } from "../../constants";
+import ImageWithFallbacks from "../ImageWithFallbacks";
 
 const Technology = (props) => {
   const { techListLength, frontmatter, body, index } = props;
   const { logoImage, readMoreSlug, key } = frontmatter;
+
+  let BaseImageURL = BASE_IMAGE_URL + `${key}`;
   let theReadMoreLink;
   let columnClass;
   if (techListLength % 2 != 0 && techListLength - 1 == index) {
@@ -24,10 +27,22 @@ const Technology = (props) => {
     );
   }
 
+  const onError = () => {
+    console.log("errored");
+  };
+
   return (
     <div className={columnClass}>
       <article className={skill} data-aos="flip-left">
         <figure>
+          <ImageWithFallbacks url={BaseImageURL}></ImageWithFallbacks>
+          {/* <img
+            src={__dirname + "/assets/images/azure.jpeg"}
+            onError={(e) => {
+              console.log("E", e);
+              onError();
+            }}
+          ></img> */}
           <GatsbyImage image={getImage(logoImage)} alt={key} />
         </figure>
         <MDXRenderer>{body}</MDXRenderer>
