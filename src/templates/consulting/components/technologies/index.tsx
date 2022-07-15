a305bcaca526e8d61463070ee60af262abc89cb4
ozairashfaqueSSW <OzairAshfaque@ssw.com.au>
2022-07-15 15:03:58 +1000

    Added todo

--- src/templates/consulting/components/technologies/index.tsx
+++ src/templates/consulting/components/technologies/index.tsx
@@ -4,7 +4,7 @@ import { useTechnologyMdxData } from "../../../../hooks/useTechnologyMdxData";
 import Technology from "../../../../components/technology";
 
 const Technologies = ({ technologies, techHeader }) => {
-  //TODO: change technologies to technologyCards after refactor ssw.website.content
+  //TODO: change technologies to technologyCards after refactor SSW.website.content
   const techList = technologies;
   const nodes = useTechnologyMdxData();
   const [techComponents, setTechComponents]: any = useState([]);
