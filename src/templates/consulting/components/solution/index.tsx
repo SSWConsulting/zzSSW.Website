fd10bdf3d0d182a9e7704b19e0b72f0af815b1ae
Ozair Ashfaque [SSW] <96504411+ozairashfaqueSSW@users.noreply.github.com>
2022-07-19 17:33:59 +1000

    Added booking form in Solution and testimonials (#238)

--- src/templates/consulting/components/solution/index.tsx
+++ src/templates/consulting/components/solution/index.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
+import BookingFormPopup from "../../../../components/bookingFormPopup";
 
-import Button from '../../../../components/button';
-import { container } from './index.module.css';
+import Button from "../../../../components/button";
+import { container } from "./index.module.css";
 
-const Solution = ({ project }) => (
-    <section className={container}>
+const Solution = ({ project }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const showBookingForm = () => setIsVisible(!isVisible);
+  return (
+    <>
+      <section className={container}>
         <article className="main-container">
-            <h1>
-                Talk to us about your <span className="red">{project}</span>{' '}
-                project
-            </h1>
+          <h1>
+            Talk to us about your <span className="red">{project}</span> project
+          </h1>
 
-            <p>
-                Jump on a call with one of our Account Managers to discuss how
-                we can help you.
-            </p>
+          <p>
+            Jump on a call with one of our Account Managers to discuss how we
+            can help you.
+          </p>
 
-            <Button>Book a FREE Initial Meeting</Button>
+          <Button onClick={showBookingForm} data-aos="fade-up">
+            Book a FREE Initial Meeting
+          </Button>
+          <BookingFormPopup
+            isVisible={isVisible}
+            showBookingForm={setIsVisible}
+            isShareForm={false}
+          />
 
-            <h2>or call us on +61 2 9953 3000</h2>
+          <h2>or call us on +61 2 9953 3000</h2>
         </article>
-    </section>
-);
+      </section>
+    </>
+  );
+};
 
 export default Solution;
